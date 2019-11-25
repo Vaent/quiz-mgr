c27b06d6878d39f3ef82a830b7dbac01cc7743b9
Al <>
2019-11-25 14:13:33 +0000

    Initialise Java web API project

---
+++ api/quiz-mgr/src/main/java/ltd/webbiskools/quizmgr/QuizMgrApplication.java
@@ -0,0 +1,13 @@
+package ltd.webbiskools.quizmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizMgrApplication.class, args);
+	}
+
+}
---
+++ api/quiz-mgr/src/test/java/ltd/webbiskools/quizmgr/QuizMgrApplicationTests.java
@@ -0,0 +1,13 @@
+package ltd.webbiskools.quizmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
