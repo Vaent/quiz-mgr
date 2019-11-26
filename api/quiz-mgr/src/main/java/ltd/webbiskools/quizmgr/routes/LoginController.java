55f9cf1e01e57cad48e3a763710c500d2a6e8ec3
Al <>
2019-11-26 10:08:16 +0000

    Serve a template HTML file

--- api/quiz-mgr/src/main/java/ltd/webbiskools/quizmgr/routes/LoginController.java
+++ api/quiz-mgr/src/main/java/ltd/webbiskools/quizmgr/routes/LoginController.java
@@ -2,11 +2,12 @@ package ltd.webbiskools.quizmgr.routes;
 
 import ltd.webbiskools.quizmgr.model.UserCredentialChecker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/login")
 public class LoginController {
 
@@ -16,7 +17,7 @@ public class LoginController {
     @GetMapping()
     public String login() {
         userCredentialChecker.check("AL", "bar");
-        return "hello world";
+        return "login";
     }
 
 }
