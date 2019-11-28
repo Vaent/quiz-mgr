b358b48009b4652a5d188475ff065332d862f9a6
Al <>
2019-11-28 17:42:12 +0000

    Reinstate logout message

--- api/quiz-mgr/src/main/java/ltd/webbiskools/quizmgr/routes/UserAuthController.java
+++ api/quiz-mgr/src/main/java/ltd/webbiskools/quizmgr/routes/UserAuthController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -23,12 +24,13 @@ public class UserAuthController {
     }
 
     @GetMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response) {
+    public String logout(ModelMap model, HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null){
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
-        return "redirect:/login";
+        model.addAttribute("logout", true);
+        return "login";
     }
 
 }
