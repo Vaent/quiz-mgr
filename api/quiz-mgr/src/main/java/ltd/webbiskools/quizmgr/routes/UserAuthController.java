9cb2ebbe56c46c9a5618aec5ee25dc52bd27d6fd
Al <>
2019-11-28 17:17:27 +0000

    Fix problem serving CSS files
    @EnableWebMvc was overriding default Spring Boot behaviour for picking
    up static files. Removing this annotation and testing all routes for all
    permissions showed that it was redundant.
    
    An exception has also been added to Spring Security config to permit
    serving CSS files when not logged in.

--- api/quiz-mgr/src/main/java/ltd/webbiskools/quizmgr/routes/UserAuthController.java
+++ api/quiz-mgr/src/main/java/ltd/webbiskools/quizmgr/routes/UserAuthController.java
@@ -2,16 +2,13 @@ package ltd.webbiskools.quizmgr.routes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @Controller
 public class UserAuthController {
 
